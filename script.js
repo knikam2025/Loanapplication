798f1b16eec1cce73923036c5f518153307af6d8
myname <abc@gmail.com>
2024-01-14 18:24:47 +0530

    added features

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.getElementById('loanForm').addEventListener('submit', function (event) {
     event.preventDefault();
-    // Perform form submission and redirect to confirm.html
     window.location.href = `confirm.html?fullName=${encodeURIComponent(document.getElementById('fullName').value)}&email=${encodeURIComponent(document.getElementById('email').value)}`;
   });
   
@@ -15,15 +14,15 @@ document.getElementById('loanForm').addEventListener('submit', function (event) 
     }
   
     
-    // Calculate Estimated EMI
+    // CalculatING EMI
     const interestRate = 8.5; // in percentage
     const tenureInYears = 15;
   
-    const monthlyInterestRate = interestRate / 1200; // Monthly interest rate
-    const numberOfPayments = tenureInYears * 12; // Total number of payments
+    const monthlyInterestRate = interestRate / 1200; // IT IS Monthly interest rate
+    const numberOfPayments = tenureInYears * 12;
   
     const emi = (loanAmount * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments));
     
     alert(`Estimated EMI: ${emi.toFixed(2)} Rs.`);
-  }
+  };
   
